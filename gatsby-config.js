a451d133554af6cd93a1d8fa43946b818c378dc8
Cody Hollis-Perdue <mr_eedok@hotmail.com>
2023-01-29 15:12:47 -0700

    Readded the code

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Whooptopia 2023 Fleet Survey`,
+    siteUrl: `https://eedok.github.io/Whooptopia2023Survey`,
+  },
+  plugins: [],
+  pathPrefix: "/Whooptopia2023Survey"
+}
